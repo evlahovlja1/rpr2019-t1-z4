dc2ad19637ebdca24b4b1fb817bd83639dbcc6c9
Elvir Vlahovljak <evlahovlja1@etf.unsa.ba>
2020-10-26 18:46:03 +0100

    pocetak razvoja klase Supermarket

--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,28 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+    private final int MAX_ARTIKALA = 1000;
+    private Artikl[] artikli = new Artikl[MAX_ARTIKALA];
+    private int brojTrenutnihArtikala = 0;
+
+    public Artikl[] getArtikli() {
+        return artikli;
+    }
+
+    void dodajArtikl(Artikl a) {
+        if (brojTrenutnihArtikala == MAX_ARTIKALA) {
+            return;
+        }
+
+        artikli[brojTrenutnihArtikala] = a;
+        brojTrenutnihArtikala = brojTrenutnihArtikala + 1;
+    }
+
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        for (Artikl a: artikli) {
+            if ()
+        }
+        return null;
+    }
 }
