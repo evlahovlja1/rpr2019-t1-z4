7354aefa180fe97f772a62320793c93543b37265
Elvir Vlahovljak <evlahovlja1@etf.unsa.ba>
2020-10-26 18:54:09 +0100

    Korpa klasa gotova

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,42 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+    private final int MAX_ARITKALA = 50;
+    private Artikl[] artikli = new Artikl[MAX_ARITKALA];
+    private int trenutniBrojArtikala = 0;
+
+    public boolean dodajArtikl(Artikl a) {
+        if (trenutniBrojArtikala == MAX_ARITKALA) {
+            return false;
+        }
+
+        artikli[trenutniBrojArtikala] = a;
+        trenutniBrojArtikala = trenutniBrojArtikala + 1;
+        return true;
+    }
+
+    public Artikl[] getArtikli() {
+        return artikli;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        for (int i = 0; i < trenutniBrojArtikala; ++i) {
+            Artikl a = artikli[i];
+
+            if (a.getKod().equals(kod)) {
+                System.arraycopy(artikli, i+1, artikli, i, trenutniBrojArtikala-i-1);
+                trenutniBrojArtikala = trenutniBrojArtikala - 1;
+                return a;
+            }
+        }
+        return null;
+    }
+
+    public int dajUkupnuCijenuArtikala() {
+        int suma = 0;
+        for (int i = 0; i < trenutniBrojArtikala; ++i) {
+            suma = suma + artikli[i].getCijena();
+        }
+        return suma;
+    }
 }
